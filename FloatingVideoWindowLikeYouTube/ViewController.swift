0be5813341b0ed796b64b8d945ceb3027bb3ab44
abhishekkhedekar <khedekarabhishek@gmail.com>
2018-01-09 23:59:41 +0530

    floating video window added

--- FloatingVideoWindowLikeYouTube/ViewController.swift
+++ FloatingVideoWindowLikeYouTube/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
+// https://stackoverflow.com/questions/24092414/ios-floating-video-window-like-youtube-app
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var tallMpContainer: UIView!
+    @IBOutlet weak var mpContainer: UIView!
+    
+    var swipeDown: UISwipeGestureRecognizer?
+    var swipeUp: UISwipeGestureRecognizer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeDownAction))
+        
+        swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeUpAction))
+        
+        swipeDown?.direction = .down
+        swipeUp?.direction = .up
+        
+        self.mpContainer.addGestureRecognizer(swipeDown!)
+        self.mpContainer.addGestureRecognizer(swipeUp!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @objc func swipeDownAction() {
+        minimizeWindow(minimized: true, animated: true)
+    }
+    
+    @objc func swipeUpAction() {
+        minimizeWindow(minimized: false, animated: true)
+    }
+    
+    func isMinimized() -> Bool {
+        return CGFloat((self.tallMpContainer?.frame.origin.y)!) > CGFloat(20)
+    }
+    
+    func minimizeWindow(minimized: Bool, animated: Bool) {
+        if isMinimized() == minimized {
+            return
+        }
+        
+        var tallContainerFrame: CGRect
+        var containerFrame: CGRect
+        
+        var tallContainerAlpha: CGFloat
+        
+        if minimized == true {
+            
+            let mpWidth: CGFloat = 160
+            let mpHeight: CGFloat = 90
+            
+            let x: CGFloat = 320-mpWidth
+            let y: CGFloat = self.view.bounds.size.height - mpHeight;
+            
+            tallContainerFrame = CGRect(x: x, y: y, width: 320, height: self.view.bounds.size.height)
+            containerFrame = CGRect(x: x, y: y, width: mpWidth, height: mpHeight)
+            tallContainerAlpha = 0.0
+            
+        } else {
+            
+            tallContainerFrame = self.view.bounds
+            containerFrame = CGRect(x: 0, y: 0, width: 320, height: 180)
+            tallContainerAlpha = 1.0
+            
+        }
+        
+        let duration: TimeInterval = (animated) ? 0.5 : 0.0
+        UIView.animate(withDuration: duration, animations: {
+            self.tallMpContainer.frame = tallContainerFrame
+            self.mpContainer.frame = containerFrame
+            self.tallMpContainer.alpha = tallContainerAlpha
+        })
+    }
+
+    
 
 }
 
